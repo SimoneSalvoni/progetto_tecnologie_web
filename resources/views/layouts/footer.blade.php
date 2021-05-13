9bdc4b76aaded79ce73d524639eba5b1c386957d
Simone Salvoni <simone.salvoni99@gmail.com>
2021-05-13 19:30:32 +0200

    Inizio controller, rotte e inizio viste

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,11 @@
+<section id="footer-bar">
+    <div class="row">
+        <div class="span3">
+            <h4>Informazioni</h4>
+            <ul class="nav">
+                <li><a href="./index.html">Chi siamo</a></li>  
+                <li><a href="./about.html">FAQ</a></li>							
+            </ul>					
+        </div>				
+    </div>	
+</section>
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,14 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+ 
+ */
+
+Route::get('/', 'PublicController@showHomePage') -> name('home');
+Route::get('/listaEventi', 'PublicController@showEventsList') -> name('lista');
+Route::get('/Evento/{eventId}', 'PublicController@showEvent') -> name('evento');
+Route::get('/info', 'PublicController@showInfo') -> name('info');
