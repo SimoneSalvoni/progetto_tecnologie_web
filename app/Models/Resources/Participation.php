3f9b09f6b537d75eca62f8c1ff54103040438925
Lorenzo Sopranzetti <s1090517@studenti.univpm.it>
2021-05-15 16:52:05 +0200

    aggiustamento partecipazione

--- app/Models/Resources/Participation.php
+++ app/Models/Resources/Participation.php
@@ -8,32 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Participation extends Model
 {
     /**
-     * Primary key della tabella associata al modello
-     * 
-     * @var string
-     */
-    protected $primarykey = 'email';
-    
-    /**
-     * Indica se la primary key è un auto increment o no
-     * 
-     * @var bool
-     */
-    public $incrementing = false;
-    
-    /**
      * Indica che il modello non devere cercare delle colonne con il time stamp
-     * 
+     *  
      * @var boolean
      */
     public $timestamps = false;
-    
-    /**
-     * Il tipo di dato della chiave
-     * 
-     * @var string
-     */
-    public $keyType = 'string';
     
     /**
      * Recupera il modello del partecipante
