858774c5347414015bdb8a123392cb0ee2844939
Lorenzo Sopranzetti <s1090517@studenti.univpm.it>
2021-05-16 20:46:35 +0200

    aggiunta di alcune fuzioni di filtraggio degli eventi

---
+++ app/Models/Filters.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Models;
+
+/**
+ * Lista dei filtri utilizzabili per la ricerca degli eventi.
+ * 
+ * @author Lorenzo
+ */
+class Filters {
+    /**
+     * Recupera gli eventi programmati in una certa data
+     * @param type $data
+     * @return type
+     */
+    public function FilterByData($data){
+        return Event::where('data','=',strval($data))->get();
+    }
+    
+    /**
+     * Recupera gli eventi organizzati da una certa società organizzatrice
+     * @param type $organizzatore
+     * @return type
+     */
+    public function FilterByOrganizzatore($organizzatore){
+        return Event::where('emailorganizzatore', '=', strval($organizzatore))->get();
+    }
+    
+    /**
+     * Recupera gli eventi che contengono nella descrizione le parole passate
+     * @param type $descrizione
+     * @return type
+     */
+    public function FilterByDescizione($descrizione) {
+        return Event::where('descrizione', '=', '%'.strval($descrizione).'%');
+    }
+    //TODO Trovare un modo per effettuare la ricerca con la regione
+}   
+
