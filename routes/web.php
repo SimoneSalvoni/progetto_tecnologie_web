6c8992b1bb06a3ec5041b3bef2e53e9974441927
RGiova <66753387+RGiova@users.noreply.github.com>
2021-05-20 15:32:59 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,23 @@ Route::get('/listaEventi', [PublicController::class,'showEventsList']) -> name('
 Route::post('/listaEventi', [PublicController::class,'showEventsListFiltered']) -> name('list.search');
 Route::get('/listaEventi/Evento/{eventId}', [PublicController::class,'showEvent']) -> name('event');
 Route::get('/info', [PublicController::class,'showInfo']) -> name('info');
+<<<<<<< Updated upstream
 //Auth::routes();
 
 //Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+=======
+/*Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');*/
+
+//login
+Route::get('login', 'Auth/LoginController@showLoginForm')
+        ->name('login');
+Route::post('login', 'Auth/LoginController@login');
+Route::get('logout', 'Auth/LoginController@logout')
+        ->name('logout');
+
+//registrazione
+Route::get('register', 'Auth/RegisterController@showRegistrationForm')
+        ->name('register');
+Route::post('register', 'Auth/RegisterController@register');
+>>>>>>> Stashed changes
