c1333ac243e9b688b0b25d7d3fba4937af380186
RGiova <66753387+RGiova@users.noreply.github.com>
2021-05-22 12:31:25 +0200

    Event blade
    Modificata event blade, ci sono dei placeholder e vanno tolte gli stili dalle div e messi su classi (Da togliere la classe 'desc' o comunque cambiarla a solo "overflow-wrap: break-word")

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -20,12 +20,32 @@
             <form class="form-inline" action="#">
                 <!-- per il GUEST DEVE LINKARE AL LOGIN-->
                 <h5>Biglietti rimanenti: {{$event->bigliettitotali-$event->bigliettivenduti}}</h5>
-                <input class="btn btn-inverse" type="submit" value="Acquista"></input>
+                
+                @guest
+                <input class="btn btn-inverse" type="submit" value="Acquista" formaction="{{ route('login') }}"></input>
+                @endguest
+
+                @can('isUser')
+                 <input class="btn btn-inverse" type="submit" value="Acquista" formaction="{{ route('logout') }}"></input>
+                 //Qua va passata la rotta di acquista con i dati dell'evento, per ora lascio logout come rotta almeno fa qualcosa di diverso
+                @endcan
+                
+                @can('isOrg')
+                //tasti modifica e cancella?
+                @endcan
             </form>
             <div class="form-inline">
                 <form class="form-inline">
                     <!-- IMPLEMENTA IL SUBMIT -->
-                    <input class="btn btn-inverse" type="submit" value="Parteciper&ograve"></input>
+                    @guest
+                    <input class="btn btn-inverse" type="submit" value="Parteciper&ograve" formaction="{{ route('login') }}"></input>
+                    @endguest
+                    
+                    @can('isUser')
+                    <input class="btn btn-inverse" type="submit" value="Parteciper&ograve" formaction="{{ route('logout') }}"></input>
+                    //Qua deve creare la partecipazione, forse va fatto da userController (come fatto per le creazioni di altri elementi)
+                    @endcan
+                    
                     <p>
                         Persone che parteciperanno: {{$event->partecipero}}
                     </p>
@@ -34,11 +54,17 @@
         </div>							 
         <div class="span9">	
             <hr>
-            <div class="container" style="width: 100%;">
-                <p class="desc">
-                    {{$event->descrizione}}
-                </p>
-                
+            <div class="container" style="width: 100%; display:flex;">
+                    <div style="display:flex; flex-direction:column; width: 50%; float: left; margin-right: 50px;">
+                        <div style="overflow-wrap: break-word;">
+                            <h5>Descrizione</h5>
+                            <p>{{$event->descrizione}}</p>
+                        </div>                                                               
+                        <div style="overflow-wrap: break-word;">
+                            <h5>Come raggiungerci</h5>
+                            <p>{{$event->indicazioni}}</p>
+                        </div>
+                    </div>
                 <div>  
                     <iframe width="400" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="{{$event->urlluogo}}"></iframe><small><a href="{{$event->urlluogo}}"""></a></small>  
                 </div>
