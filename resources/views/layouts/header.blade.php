06d8e2b5654ffbea3ebdaa4406c8477477929216
Daniele Staffolani <danielestaffolani@libero.it>
2021-05-23 11:19:51 +0200

    aggiunto org.blade

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,18 +1,10 @@
-<section id="header">
-    <div id="top-bar" class="container">
-        <div class="row_header" >
-<<<<<<< HEAD
-            <img src="{{ asset('siteimgs/logo.png') }}" class="site_logo" alt="" href="{{route('home')}}">
-=======
             <a href='{{route('home')}}' style='height:inherit'><img src="{{ asset('siteimgs/logo.png') }}" alt=""></a>
->>>>>>> b43d0323975591738b78d6ae21d9db7f55fa613c
             <div class="right_header">
                 <div class="account pull-right">
                     <ul class="user-menu">
                         @guest
                         <li><a href="{{route('login')}}">Accedi</a></li>
                         @endguest
-
                         @can('isUser')
                         <li><a href="{{route('areariservata.user')}}">Area utente</a></li>
                         <li><a href="" title="Esci dal sito" class="highlight" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a></li>
@@ -37,7 +29,4 @@
             </div>
         </div>
     </div>
-   <!-- <div id="wrapper" class="container">-->
-        <!--  </div> -->
 </section>
-
---
+++ resources/views/org.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.public')
+
+<h3>
+    Informazioni organizzazione
+</h3>
+
+<div class="raw">
+    <p>Nome organizzazione: {{$user->organizzazione}} <p>
+    <p>Email di riferimento: {{$user->email}}<p>
+    <p>Password: {{$user->password}} <p>
+        <button class="button" onclick="location.href = '{{route('newevent')}}'" type="button" > <b>CREA UN NUOVO EVENTO</b></button>
+    <hr size="3" color="black" style="heinght:0.5px">
+    <h4><span>Eventi imminenti</span></h4>
+
+
+    @for($i=0;$i<2;$i++)
+    @if(@isset($nearEvents[$i])
+    <section class="single_product">
+        <div class="product_container clickable"; onclick="location.href ='{{route('event',[$event->id])}}'">
+            <!--<div class="image_item"><img src="concert.jpg" alt="Immagine dell'evento" class="product_image"></div>-->
+            <div class="image_item"> <img src="{{asset('locandine/'.$nearEvents[$i]->immagine)}}" class="product_image"></div>
+            <div class="descr_container">
+                <div class="title_item"><h4>{{$event->nome}}
+                    </h4></div>
+                <div class="info-container">
+                    <div>LUOGO: {{route('event',[$nearEvents[$i]->regione])}}</div>
+                    <div>DATA: {{route('event',[$nearEvents[$i]->data])}}</div>
+                    <div>BIGLIETTI VENDUTI: </div>
+                    <div>INCASSO: </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    @endif
+    @endfor
+
+
+
+
+                /* 
+                * To change this license header, choose License Headers in Project Properties.
+                * To change this template file, choose Tools | Templates
+                * and open the template in the editor.
+                */
+
