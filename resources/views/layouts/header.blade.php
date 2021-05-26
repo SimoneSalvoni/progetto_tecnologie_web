440335bac4add00b2b1a3ceec29be962ff24cf8a
Daniele Staffolani <danielestaffolani@libero.it>
2021-05-26 15:17:29 +0200

    modificato header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,3 +1,6 @@
+<section id="header">
+    <div id="top-bar" class="container">
+        <div class="row_header" >
             <a href='{{route('home')}}' style='height:inherit'><img src="{{ asset('siteimgs/logo.png') }}" alt=""></a>
             <div class="right_header">
                 <div class="account pull-right">
