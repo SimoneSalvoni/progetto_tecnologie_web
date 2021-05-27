d5f72f2468c9b78f04a8e95c0c7d4f291f055142
Daniele Staffolani <danielestaffolani@libero.it>
2021-05-27 17:30:33 +0200

    inserita richiesta di diventare organizzatore

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -52,6 +52,9 @@
         </div>
         @endisset
         <button class="button clickable" onclick="location.href = '{{route('list')}}'" type="button" > <b>VAI ALLA LISTA COMPLETA DEGLI EVENTI</b></button>
+        <div style="margin-top: 2em">
+        <h5> Nel caso volessi organizzare eventi nel sito contatta l'amministratore alla mail <a href="mailto:admin@r3stickets.it?subject=Richiesta nuovo organizzatore.&body=Salve con la presente chiedo maggiori informazioni sul ruolo di organizzatore all'interno del sito."> admin@r3stickets.it </a></h5>
+        </div>
     </section>
 <!--</div>-->
 @endsection 
