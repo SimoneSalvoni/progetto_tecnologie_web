58389d992418f2829de07a3a5443595e0298a953
RGiova <66753387+RGiova@users.noreply.github.com>
2021-06-04 15:04:10 +0200

    Faq migliorate e scritte + info

--- database/seeds/FaqSeeder.php
+++ database/seeds/FaqSeeder.php
@@ -13,11 +13,41 @@ class FaqSeeder extends Seeder
      */
     public function run()
     {
-        for ($i=1; $i<=3; $i++){
+        
             DB::table('faqs')->insert([
-               'domanda' => 'Domanda numero '.$i,
-                'risposta' => 'Risposta numero '.$i
+               'domanda' => 'Ho acquistato un biglietto di troppo, è possibile chiedere un rimborso?',
+                'risposta' => 'Sì, tuttavia ti invitiamo a contattarci tramite mail per questo tipo di procedura'
             ]);
-        }
+            
+            DB::table('faqs')->insert([
+               'domanda' => 'Vorrei accorpare più ordini, è possibile?',
+                'risposta' => 'No, il nostro sistema non permette questo tipo di operazione'
+            ]);
+            
+            DB::table('faqs')->insert([
+               'domanda' => 'Quali sono i costi del servizio?',
+                'risposta' => 'Non ci sono costi di servizio per i clienti in quanto R3S offre solamente biglietti digitali'
+            ]);
+            
+            DB::table('faqs')->insert([
+               'domanda' => 'Posso usare il Bonus Cultura riservato ai diciottenni sul sito R3S?',
+                'risposta' => 'No, in quanto il nostro esercizio ha ancora meno di un anno di attività'
+            ]);
+            
+            DB::table('faqs')->insert([
+               'domanda' => 'Come procedo per registrarmi al sito?',
+                'risposta' => 'Da qualsiasi pagina del sito basterà cliccare in alto a destra su "Accedi", dopodiché cliccare "Registrati", subito sotto il tasto di accesso e seguire la procedura'
+            ]);
+            
+            DB::table('faqs')->insert([
+               'domanda' => 'Quali possibili modalità di pagamento ci sono?',
+                'risposta' => 'Ad oggi i metodi di pagamento consentiti sono PostePay, Paypal e conto bancario'
+            ]);
+            
+            DB::table('faqs')->insert([
+               'domanda' => 'Come posso raggiungere il servizio clienti?',
+                'risposta' => 'Tramite mail al seguente indirizzo: servizioclienti@r3stickets.it'
+            ]);
+        
     }
 }
